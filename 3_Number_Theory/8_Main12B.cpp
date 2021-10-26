6a435eee1245f472adffadebeef5574386dbb6de
Muhammad <alaminmir08@gmail.com>
2021-10-26 20:48:35 +0600

    solve another problem

---
+++ 3_Number_Theory/8_Main12B.cpp
@@ -0,0 +1,227 @@
+/*
+    Problem link: https://www.spoj.com/problems/MAIN12B/
+    solution:
+ */
+// In the name of ALLAH
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define PB push_back
+#define F first
+#define S second
+#define MP make_pair
+#define endl '\n'
+#define all(a) (a).begin(), (a).end()
+#define sz(x) (int)x.size()
+#define mid(l, r) ((r + l) / 2)
+#define left(node) (node * 2)
+#define right(node) (node * 2 + 1)
+#define mx_int_prime 999999937
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+
+#define MOD 1000000007
+#define mem(a, b) memset(a, b, sizeof(a))
+#define gcd(a, b) __gcd(a, b)
+ll lcm(ll a, ll b)
+{
+    return a * (b / gcd(a, b));
+}
+#define sqr(a) ((a) * (a))
+
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fraction(a)               \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(a);            \
+    cout.setf(ios::fixed, ios::floatfield);
+#define file()                        \
+    freopen('input.txt', 'r', stdin); \
+    freopen('output.txt', 'w', stdout);
+
+typedef vector<int>::iterator vit;
+typedef set<int>::iterator sit;
+
+// int dx[] = {0, 0, +1, -1};
+// int dy[] = {+1, -1, 0, 0};
+// int dx[] = {+1, 0, -1, 0, +1, +1, -1, -1};
+// int dy[] = {0, +1, 0, -1, +1, -1, +1, -1};
+
+template <typename F, typename S>
+ostream &operator<<(ostream &os, const pair<F, S> &p)
+{
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << '{';
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        if (it != v.begin())
+            os << ',';
+        os << *it;
+    }
+    return os << ',' << ' ';
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const set<T> &v)
+{
+    os << '[';
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        if (it != v.begin())
+            os << ',';
+        os << *it;
+    }
+    return os << ']';
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const multiset<T> &v)
+{
+    os << '[';
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        if (it != v.begin())
+            os << ',';
+        os << *it;
+    }
+    return os << ']';
+}
+
+template <typename F, typename S>
+ostream &operator<<(ostream &os, const map<F, S> &v)
+{
+    os << '[';
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        if (it != v.begin())
+            os << ',';
+        os << it->first << ' ' << '=' << ' ' << it->second;
+    }
+    return os << ']';
+}
+
+#define dbg(args...)                        \
+    do                                      \
+    {                                       \
+        cerr << #args << ' ' << ':' << ' '; \
+        fanc(args);                         \
+    } while (0)
+void fanc()
+{
+    cerr << endl;
+}
+
+template <typename T>
+void fanc(T a[], int n)
+{
+    for (int i = 0; i < n; ++i)
+        cerr << a[i] << ' ';
+    cerr << endl;
+}
+
+template <typename T, typename... hello>
+void fanc(T arg, const hello &...rest)
+{
+    cerr << arg << ' ';
+    fanc(rest...);
+}
+const int mx = 1e6 + 123;
+bitset<mx> isPrime;
+vi primes;
+void primeGen(int n)
+{
+    n += 100;
+    for (int i = 3; i <= n; i += 2)
+        isPrime[i] = 1;
+    int sq = sqrt(n);
+
+    for (int i = 3; i <= sq; i += 2)
+    {
+        if (isPrime[i])
+        {
+            for (int j = i * i; j <= n; j += (i + i))
+            {
+                isPrime[j] = 0;
+            }
+        }
+    }
+    isPrime[2] = 1;
+    primes.PB(2);
+    for (int i = 3; i <= n; i += 2)
+    {
+        if (isPrime[i])
+            primes.PB(i);
+    }
+}
+vector<long long> prime_factorization(long long n)
+{
+
+    vector<ll> res;
+
+    for (auto p : primes)
+    {
+        if (1ll * p * p > n)
+            break;
+        if (n % p == 0)
+        {
+
+            res.PB(p);
+            while (n % p == 0)
+            {
+                n /= p;
+            }
+        }
+    }
+    if (n > 1)
+        res.PB(n);
+    return res;
+}
+
+int main()
+{
+    optimize();
+    primeGen(1e6);
+    int t;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++)
+    {
+        int n;
+        cin >> n;
+        set<ll> primeFactors;
+        for (int i = 1; i <= n; i++)
+        {
+            ll a;
+            cin >> a;
+            vl res = prime_factorization(a);
+            for (auto u : res)
+                primeFactors.insert(u);
+        }
+
+        cout << "Case #" << tc << ": " << primeFactors.size() << endl;
+        for (auto p : primeFactors)
+            cout << p << endl;
+    }
+
+    return 0;
+}
